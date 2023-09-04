ef2fe45319548e94b13e1c88a3c2b7345b46d734
fing1499 <findlayg@gmail.com>
2023-09-04 16:55:49 +0100

    fixing group schemas

--- models/group.js
+++ models/group.js
@@ -1,13 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const groupSchema = new Schema({
-  name: { type: String, required: true },
-  invite_key: { type: String },
-  users: { type: Array }
+const weeklySelectionSchema = new Schema({
+  team: { type: String, required: true },
+  result: { type: String, required: true }
 })
 
+const winlossHistorySchema = new Schema({
+  user: { type: String },
+  weeklySelections: { type: [weeklySelectionSchema] }
+})
 
+const groupSchema = new Schema({
+  name: { type: String, required: true },
+  invite_key: { type: String, required: true, unique: true },
+  users: [{
+    user: { type: String },
+    stillPlaying: { type: Boolean, default: true }
+  }],
+  history: { type: [winlossHistorySchema] }
+})
 
-
-module.exports = mongoose.model('Group', groupSchema);
+module.exports = {
+  Group: mongoose.model('Group', groupSchema),
+  WinLoss: mongoose.model('WinLoss', winlossHistorySchema),
+  WeeklySelection: mongoose.model('WeeklySelection', weeklySelectionSchema)
+};
